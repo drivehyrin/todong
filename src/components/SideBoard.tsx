533bfa02b6d46d839bad8f342b3af335fd99480c
drivehyrin <drivehyrin@gmail.com>
2024-08-24 19:53:29 +0900

    feat: sidebar 컴포넌트 기본 레이아웃 구현 desc - 기능은 넣지 않고 sideboard에 표시될 아이템(버튼) ui 구현 - 버튼이 다른 컴포넌트에서 사용될지 아직 모르므로 우선 SideBoard 컴포넌트 내에 구현

--- src/components/SideBoard.tsx
+++ src/components/SideBoard.tsx
@@ -2,11 +2,38 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 280px;
+  padding: 15px;
   background-color: rgb(70, 130, 169, 0.2);
 `;
 
+const Item = styled.div`
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 5px;
+
+  &:hover {
+    cursor: pointer;
+    transition: 0.5s ease-out;
+    background-color: rgb(70, 130, 169, 0.3);
+  }
+`;
+
+const VerticalBar = styled.div`
+  background-color: rgb(70, 130, 169, 0.3);
+  height: 1px;
+  margin: 5px 0px;
+`;
+
 function SideBoard() {
-  return <Wrapper />;
+  return (
+    <Wrapper>
+      <Item>접속한 계정명</Item>
+      <Item>작업 추가</Item>
+      <VerticalBar />
+      <Item>오늘</Item>
+      <Item>다음</Item>
+    </Wrapper>
+  );
 }
 
 export default SideBoard;
